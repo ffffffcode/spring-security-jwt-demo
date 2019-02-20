70371929b7bca01bb001198e57f0807d4d054b2a
AaronFae <AaronFae@163.com>
2019-02-20 23:26:52 +0800

    Update code

---
+++ src/main/java/ex/aaronfae/graduation/model/vo/LoginVO.java
@@ -0,0 +1,30 @@
+package ex.aaronfae.graduation.model.vo;
+
+/**
+ * 登录实体类
+ * 
+ * @author AaronFae
+ *
+ */
+public class LoginVO {
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
